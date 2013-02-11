e8f0cfc28ade0ddcfc5b7218f8cfd5ee132cf5db
cqcn1991 <dadazhu_ashi@163.com>
2013-02-11 20:51:31 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Greissue::Application.config.secret_token = '40270f33581f6469364dafeb4fb892065450484674d0ee442f498db8046e38fc4aa53aab1b24d91cf91bbdfb3e94ee12337634734d3389ab6a83375ba1e4343d'
